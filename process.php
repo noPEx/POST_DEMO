7492ce3764b2bf5b62ca8963ce52599ecdf75d54
Soumya Mandi <soumyamandi@gmail.com>
2013-08-27 20:34:33 +0530

    working example.

---
+++ process.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+<?php
+    echo "Hello World, we received the form?";
+    $first = $_POST['word'];
+    $second = $_POST['number'];
+    echo "first is : $first\n";
+    echo "second is : $second";
+?>
+    </body>
+</html>
